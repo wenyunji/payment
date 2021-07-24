ba644b62dba861f77c5df972811200369b5dd041
wenyunji <463206962@qq.com>
2021-07-24 18:23:34 +0800

    git push -u origin

--- go.mod
+++ go.mod
@@ -3,10 +3,19 @@ module github.com/wenyunji/payment
 go 1.16
 
 require (
-	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/HdrHistogram/hdrhistogram-go v1.1.0 // indirect
+	github.com/golang/protobuf v1.4.3
+	github.com/jinzhu/gorm v1.9.16
+	github.com/micro/go-micro/v2 v2.9.1
 	github.com/micro/go-plugins/config/source/consul/v2 v2.9.1 // indirect
+	github.com/micro/go-plugins/registry/consul/v2 v2.9.1 // indirect
+	github.com/micro/go-plugins/wrapper/monitoring/prometheus/v2 v2.9.1 // indirect
+	github.com/micro/go-plugins/wrapper/ratelimiter/uber/v2 v2.9.1 // indirect
+	github.com/micro/go-plugins/wrapper/trace/opentracing/v2 v2.9.1 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/uber/jaeger-client-go v2.29.1+incompatible // indirect
 	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
-	github.com/wenyunji/common v0.0.0-20210724044708-001c99b33387 // indirect
+	github.com/wenyunji/common v0.0.0-20210724044708-001c99b33387
+	google.golang.org/protobuf v1.26.0-rc.1
+
 )
--- main.go
+++ main.go
@@ -1,17 +1,80 @@
 package main
 
 import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-plugins/registry/consul/v2"
+	"github.com/micro/go-plugins/wrapper/monitoring/prometheus/v2"
+	ratelimit "github.com/micro/go-plugins/wrapper/ratelimiter/uber/v2"
+	opentracing2 "github.com/micro/go-plugins/wrapper/trace/opentracing/v2"
+	"github.com/opentracing/opentracing-go"
+	"github.com/wenyunji/common"
+	"github.com/wenyunji/payment/domain/repository"
+	service2 "github.com/wenyunji/payment/domain/service"
+	"github.com/wenyunji/payment/handler"
+	"github.com/wenyunji/payment/proto/payment"
 	"log"
 )
 
 func main() {
+	//配置中心
+	consulConfig, err := common.GetConsulConfig("127.0.0.1", 8500, "micro/config")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//注册中心
+	consul := consul.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{
+			"127.0.0.0:8500",
+		}
+	})
+
+	//jaeger 链路追踪
+	t, io, err := common.NewTracer("go.micro.service.payment", "localhost:6831")
+	if err != err {
+		log.Fatal(err)
+	}
+	defer io.Close()
+	opentracing.SetGlobalTracer(t)
+
+	//mysql 设置
+	mysqlInfo := common.GetMysqlFromConsul(consulConfig, "mysql")
+	//初始化数据库
+	db, err := gorm.Open("mysql", mysqlInfo.User+":"+mysqlInfo.Password+"@/"+mysqlInfo.Database+"?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	//禁止复数表
+	db.SingularTable(true)
+
+	//创建表
+	tableInit := repository.NewPaymentRepository(db)
+	tableInit.InitTable()
+
+	//监控
+	common.PrometheusBoot(9089)
+
 	// Create service
 	srv := micro.NewService(
 		micro.Name("payment"),
 		micro.Version("latest"),
+		micro.Address("0.0.0.0:8089"),
+		//注册中心
+		micro.Registry(consul),
+		//添加链路追踪
+		micro.WrapHandler(opentracing2.NewHandlerWrapper(opentracing.GlobalTracer())),
+		//加载限流
+		micro.WrapHandler(ratelimit.NewHandlerWrapper(1000)),
+		//加载监控 NewHandlerWrapper
+		micro.WrapHandler(prometheus.NewHandlerWrapper()),
 	)
 
-	// Register handler
+	srv.Init()
+	paymentService := service2.NewPaymentService(repository.NewPaymentRepository(db))
+	payment.RegisterPaymentHandler(srv.Server(), &handler.Payment{Payment: paymentService})
 
 	// Run service
 	if err := srv.Run(); err != nil {
